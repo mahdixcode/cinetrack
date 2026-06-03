4398685d4b2dadd5138428a4d150e93fec664d46
“mahdixcode” <mahdixcode@gmail.com>
2026-06-03 18:12:18 +0330

    Add genres and movie_genres pipeline

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ import time
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv("/Users/mahdi/Desktop/cinetrack/.env")
 
 API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://api.themoviedb.org/3"
@@ -13,8 +13,6 @@ def get_popular_movies():
     params = {"api_key": API_KEY, "language": "en-US", "page": 1}
     response = requests.get(url, params=params)
     return response.json()
-
-data = get_popular_movies()
 
 def parse_movies(raw_movies):
     parsed = []
@@ -27,8 +25,9 @@ def parse_movies(raw_movies):
         })
     return parsed
 
-movies = data["results"]
-parsed_movies = parse_movies(movies)
+data = get_popular_movies()
+raw_movies = data["results"]
+parsed_movies = parse_movies(raw_movies)
 
 def get_movie_credits(movie_id):
     url = f"{BASE_URL}/movie/{movie_id}/credits"
@@ -42,3 +41,15 @@ def get_directors(movie_id):
     crew = credits["crew"]
     directors = [person for person in crew if person["job"] == "Director"]
     return directors
+
+def get_genres():
+    url = f"{BASE_URL}/genre/movie/list"
+    params = {"api_key": API_KEY, "language": "en-US", "page": 1}
+    response = requests.get(url , params=params)
+    time.sleep(0.3)
+    genres_data = response.json()
+    return genres_data["genres"]
+
+for i in data["results"]:
+    print(i["id"])
+    print(i["genre_ids"])
--- load.py
+++ load.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 import psycopg2
 import time
-from extract import get_popular_movies, parse_movies, get_movie_credits, get_directors
+from extract import get_popular_movies, parse_movies, get_movie_credits, get_directors, get_genres
+
 load_dotenv("/Users/mahdi/Desktop/cinetrack/.env")
 print(os.getenv("DB_USER"))
 conn = psycopg2.connect(
@@ -15,6 +16,7 @@ conn = psycopg2.connect(
 cursor = conn.cursor()
 print("YES DATABASE IS CONNECTED!")
 
+# --- Insert Movies ---
 def insert_movies(movies):
     for movie in movies:
         cursor.execute("""
@@ -31,6 +33,8 @@ data = get_popular_movies()
 movies = parse_movies(data["results"])
 insert_movies(movies)
 
+# --- Insert Directors ---
+
 def insert_directors():
     cursor.execute("SELECT id FROM movies")
     movie_ids = cursor.fetchall()
@@ -39,27 +43,76 @@ def insert_directors():
         movie_id = row[0]
         directors = get_directors(movie_id)
         
+        directors_data = []
+        movie_directors_data = []
+        
         for director in directors:
-            cursor.execute(
+            directors_data.append((director["id"], director["name"]))
+            movie_directors_data.append((movie_id, director['id']))
+        
+        cursor.executemany(
               """
                 INSERT INTO directors (id, name)
                 VALUES (%s, %s)
                 ON CONFLICT (id) DO NOTHING
               """
-                ,(director['id'], director['name'])
+                ,directors_data
             )
             
-            cursor.execute(
+        cursor.executemany(
                 """
                 INSERT INTO movie_directors (movie_id, director_id)
                 VALUES (%s,%s)
                 ON CONFLICT (movie_id, director_id) DO NOTHING
                 """
-                ,(movie_id, director['id'])
+                ,movie_directors_data
             )
         
         time.sleep(0.3)
+        
+    print("DIRECTORS HAS BEEN INSERTED!")
     
     conn.commit()
     
-insert_directors()
+# insert_directors()
+
+# --- Insert Genres ---
+def insert_genres():
+    
+    genres = get_genres()
+    
+    for genre in genres:
+        genres_query = """ INSERT INTO genres (id, name) 
+        VALUES (%s, %s) ON CONFLICT (id) DO NOTHING """
+        genres_values = (genre["id"], genre["name"])
+        cursor.execute(genres_query, genres_values)
+        
+    print("GENERES HAS BEEN INSERTED!")
+    
+    conn.commit()
+
+insert_genres()
+
+
+# --- Insert Movie_ID & Genres_IDS ---
+
+def insert_movie_genres():
+    data = get_popular_movies()
+    movies = data["results"]
+    
+    for movie in movies:
+        movie_id = movie["id"]
+        genre_ids = movie["genre_ids"]
+        
+        for genre_id in genre_ids:
+            genre_id_query = """ INSERT INTO movie_genres(movie_id, genre_id)
+            VALUES (%s, %s) 
+            ON CONFLICT (movie_id, genre_id) DO NOTHING """
+            genre_values = (movie_id, genre_id)
+        
+            cursor.execute(genre_id_query, genre_values)
+        
+    print("Movie_ID & Genres_IDS HAVE BEEN INSERTED!")
+    conn.commit()    
+
+insert_movie_genres()
